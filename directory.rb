9df2de1b62abf59589eae916177a87ba0b692307
Nicolas Bermejo <nicbermejo@gmail.com>
2021-01-28 08:56:32 +0000

    step 8

--- directory.rb
+++ directory.rb
@@ -6,29 +6,60 @@ def input_students
   students = []
   # get the first name
   name = gets.chomp
+  puts "what is your hobby"
+  hobby = gets.chomp
+  hobby = "undefined" if hobby.empty?
+  #get another name from the user
+  puts "where you from"
+  country = gets.chomp
+  country = "undefined" if country.empty?
+  puts "what is your height"
+  height = gets.chomp
+  height = "undefined" if height.empty?
   # while the name is not empty, repeat this code
+  puts "what is your cohort"
+  cohort = gets.chomp
+  cohort = "november" if cohort.empty?
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: :november}
+    students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
     puts "Now we have #{students.count} students"
-    #get another name from the user
     name = gets.chomp
+    break if name.empty?
+    puts "what is your hobby"
+    hobby = gets.chomp
+    hobby = "undefined" if hobby.empty?
+    #get another name from the user
+    puts "where you from"
+    country = gets.chomp
+    country = "undefined" if country.empty?
+    puts "what is your height"
+    height = gets.chomp
+    height = "undefined" if height.empty?
+    puts "what is your cohort"
+    cohort = gets.chomp
+    cohort = "november" if cohort.empty?
   end
-  # return the array of students
   students
 end
 # Here we define the methods to print the name
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(148)
+  puts "-------------".center(148)
 end
 def print(students)
-  students.each { |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
-    }
+  student_counter = 1
+  index = 0
+  while index <= students.length - 1
+    new_array = students[index]
+      puts "#{student_counter} #{new_array[:name]} (#{new_array[:cohort]} cohort) (hobby: #{new_array[:hobby]}) (country: #{new_array[:country]}) (height(feet): #{new_array[:height]})".center(148)
+    student_counter += 1
+    index += 1
+  end
 end
 def print_footer(students)
-  puts "Overall, we have #{students.length} great students"
+  puts "-------------".center(148)
+  puts "Overall, we have #{students.length} great students".center(148)
 end
 # Here we call the methods
 students = input_students
