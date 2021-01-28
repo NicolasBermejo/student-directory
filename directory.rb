3da0347e8ed0116960c3f70c4c3f5b2516a620cb
Nicolas Bermejo <nicbermejo@gmail.com>
2021-01-28 21:15:02 +0000

    step 10

--- directory.rb
+++ directory.rb
@@ -1,72 +1,65 @@
+@students = []
 # Here is the method to get user input
 def input_students
   puts "Please enter the name of the students"
   puts "To finish, just hit return twice"
-  # create an empty array
-  students = []
   # get the first name
   name = gets.chomp
-  puts "what is your hobby"
-  hobby = gets.chomp
-  hobby = "undefined" if hobby.empty?
-  #get another name from the user
-  puts "where you from"
-  country = gets.chomp
-  country = "undefined" if country.empty?
-  puts "what is your height"
-  height = gets.chomp
-  height = "undefined" if height.empty?
   # while the name is not empty, repeat this code
-  puts "what is your cohort"
-  cohort = gets.chomp
-  cohort = "november" if cohort.empty?
   while !name.empty? do
     # add the student hash to the array
-    students << {name: name, cohort: cohort, hobby: hobby, country: country, height: height}
-    puts "Now we have #{students.count} students"
-    name = gets.chomp
-    break if name.empty?
-    puts "what is your hobby"
-    hobby = gets.chomp
-    hobby = "undefined" if hobby.empty?
+    @students << {name: name, cohort: :november}
+    puts "Now we have #{@students.count} students"
     #get another name from the user
-    puts "where you from"
-    country = gets.chomp
-    country = "undefined" if country.empty?
-    puts "what is your height"
-    height = gets.chomp
-    height = "undefined" if height.empty?
-    puts "what is your cohort"
-    cohort = gets.chomp
-    cohort = "november" if cohort.empty?
+    name = gets.chomp
   end
-  students
+  # return the array of students
+  @students
 end
 # Here we define the methods to print the name
 def print_header
-  puts "The students of Villains Academy".center(148)
-  puts "-------------".center(148)
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-def print(students)
-  student_counter = 1
-  index = 0
-  while index <= students.length - 1
-    new_array = students[index]
-      puts "#{student_counter} #{new_array[:name]} (#{new_array[:cohort]} cohort) (hobby: #{new_array[:hobby]}) (country: #{new_array[:country]}) (height(feet): #{new_array[:height]})".center(148)
-    student_counter += 1
-    index += 1
+def print_list
+  @students.each_with_index { |student, index|
+    puts "#{student[index]} #{student[:name]} (#{student[:cohort]} cohort)"
+    }
+end
+def print_footer
+  puts "Overall, we have #{@students.length} great students"
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1"
+      input_students
+    when "2"
+      show_students
+    when "9"
+      exit
+    else
+      puts "I don't know what you mean, try again"
   end
 end
-def print_footer(students)
-  puts "-------------".center(148)
-  if students.length < 2
-    puts "Overall, we have #{students.length} great student".center(148)
-  else
-    puts "Overall, we have #{students.length} great students".center(148)
+
+def interactive_menu
+  loop do
+    print_menu
+    process(gets.chomp)
   end
 end
 # Here we call the methods
-students = input_students
-print_header
-print(students)
-print_footer(students)
+interactive_menu
